628d6cd83fcc526ddaf60250f586972e380d06b5
AlwaysEast <lojith@proton.me>
2024-10-18 00:21:57 +0100

    Create UIElement.h

---
+++ src/UI/UIElement.h
@@ -0,0 +1,23 @@
+#ifndef _UIELEMENT_H_
+#define _UIELEMENT_H_
+
+#include "SceneObject.h"
+#include "ResourceRepository.h"
+
+class UIElement : public SceneObject {
+    public:
+        UIElement();
+        ~UIElement();
+
+        void Draw() const override;
+        void HandleInput() const override;
+        void dispose() const override;
+
+    public:
+        int flag = -1;
+
+    private:
+        Vector2i Size;
+};
+
+#endif
