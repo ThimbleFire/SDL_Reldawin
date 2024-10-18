9762adf3c483e82634c1357ac837a0ce11163f76
AlwaysEast <lojith@proton.me>
2024-10-18 01:37:06 +0100

    Update Button.h

--- src/UI/Button.h
+++ src/UI/Button.h
@@ -3,21 +3,37 @@
 
 class Button : public UIElement {
     public:
-        Button();
+        Button(Transform* parentTransform) {
+            this->parentTransform = parentTransform;
+            destRect = { 
+                transform.position.x + parentTransform->position.x, 
+                transform.position.y + parentTransform->position.y, 
+                Size.x, 
+                Size.y 
+            };
+            srcRect = { 
+                0, 
+                0, 
+                32, 
+                32 
+            }
+        }
         ~Button();
 
         void Draw() const override {
-            
+            destRect.x -= camera.getCameraRect().x;
+            destRect.y -= camera.getCameraRect().y;
+            SDL_RenderCopy(g_resourceRepository.renderer, spriteTexture, &srcRect, &destRect);
         }
 
         void HandleInput() const override {
-            
         }
 
         void dispose() const override {
-            
         }
-        
+    private:
+        SDL_Rect srcRect;
+        SDL_Rect destRect;        
 };
 
 #endif
