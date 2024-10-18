744b91d5aaa7f8fe9595544d4b23f94040519fa6
AlwaysEast <lojith@proton.me>
2024-10-18 02:07:16 +0100

    Update Button.h

--- src/UI/Button.h
+++ src/UI/Button.h
@@ -3,26 +3,6 @@
 
 class Button : public UIElement {
     public:
-        Button(Transform* parentTransform) {
-            this->parentTransform = parentTransform;
-            destRect = { 
-                transform.position.x + parentTransform->position.x, 
-                transform.position.y + parentTransform->position.y, 
-                Size.x, 
-                Size.y 
-            };
-            srcRect = { 
-                0, 
-                0, 
-                32, 
-                32 
-            };
-        }
-
-        void Draw() const override {
-            SDL_RenderCopy(g_resourceRepository.renderer, spritesheet, &srcRect, &destRect);
-        }
-
         void HandleInput(InputEvent& event) const override {
             if(event.handled) return;
             
@@ -31,10 +11,7 @@ class Button : public UIElement {
                 printf("Clicked!\n");
                 event.handled = true;
             }
-        }
-    private:
-        SDL_Rect srcRect;
-        SDL_Rect destRect;        
+        }    
 };
 
 #endif
