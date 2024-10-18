26b26dd0cfb87fa0cb9a978337e9b9ebfb0be1ce
AlwaysEast <lojith@proton.me>
2024-10-18 01:18:24 +0100

    Create Button.h

---
+++ src/UI/Button.h
@@ -0,0 +1,23 @@
+#ifndef _BUTTON_H_
+#define _BUTTON_H_
+
+class Button : public UIElement {
+    public:
+        Button();
+        ~Button();
+
+        void Draw() const override {
+            
+        }
+
+        void HandleInput() const override {
+            
+        }
+
+        void dispose() const override {
+            
+        }
+        
+};
+
+#endif
