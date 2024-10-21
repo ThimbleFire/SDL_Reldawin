1d653f39905811cf563ab04c86aee15b28b70e71
AlwaysEast <lojith@proton.me>
2024-10-22 00:11:10 +0100

    Update TileMaster.h

--- src/TileMaster.h
+++ src/TileMaster.h
@@ -27,10 +27,27 @@ class TileMaster : public SceneObject {
             }
         }
 
+        void onChunkChange(Vector2i new_chunk, Vector2i last_chunk) {
+            var directionOfTravel = new_chunk - last_chunk;
+            for(int i = -1; i < 2; i++) {
+                Vector2i offset_index = Vector2i(directionOfTravel.x == 0 ? i : directionOfTravel.x, directionOfTravel.y == 0 ? i : directionOfTravel.y);
+                Vector2i create_index = new_chunk + offset_index;
+                Vector2i remove_index = old_chunk - offset_index;
+                replaceChunk(remove_index, create_index);
+            }
+        }
+
         void Draw() const override {
             for(const auto& pair : stuff) {
                 pair.second->Draw();
             }
+        }
+
+        bool isOrthogonalDirection(Vector2i coordinates) {
+            return coordinates.x == 1 && coordinates.y == 0 || coordinates.x == 0 && coordinates.y == 1 || coordinates.x == -1 && coordinates.y == 0 || coordinates.x == 0 && coordinates.y == -1;
+        }
+        bool isDiagonalDirection(coordinates : Vector2i) {
+        	return coordinates == Vector2i::ONE || coordinates == -Vector2i::ONE || coordinates.x == 1 && coordinates.y == -1 || coordinates.x == -1 && coordinates.y == 1;
         }
 
         std::map<Vector2i, TileMap*> stuff;
