d7605320d6e722f50236741021b5aef970dda53b
AlwaysEast <lojith@proton.me>
2024-10-24 00:56:03 +0100

    Update Pathfinding.cpp

--- src/Engine/Pathfinding.cpp
+++ src/Engine/Pathfinding.cpp
@@ -2,6 +2,14 @@
 
 Pathfinding g_pathfinder;
 
+struct Vector2iHash {
+    size_t operator()(const Vector2i& v) const {
+        return hash<int>()(v.x) ^ hash<int>()(v.y);
+    }
+};
+
+unordered_map<Vector2i, Node*, Vector2iHash> nodes;
+
 Pathfinding::Pathfinding() {
 
 }
@@ -10,6 +18,11 @@ Pathfinding::~Pathfinding() {
 
 }
 
-void Pathfinding::populate(std::map<Vector2i, TileMap*> stuff){
-
+void Pathfinding::populate(std::map<Vector2i, TileMap*> tilemaps){
+    for (const auto& tilemap : tilemaps) {
+        for (const auto& n : tilemap.second->tiles) {
+            Vector2i worldPos = n.first;
+            //nodes[worldPos] = n.get();  // Use raw pointer of unique_ptr for the map
+        }
+    }
 }
