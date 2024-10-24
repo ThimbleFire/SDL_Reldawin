2033a16b92e412915733e64ea521f151bb9e303d
AlwaysEast <lojith@proton.me>
2024-10-24 02:10:56 +0100

    Update Pathfinding.h

--- src/Engine/Pathfinding.h
+++ src/Engine/Pathfinding.h
@@ -2,12 +2,25 @@
 #define _PATHFINDING_H_
 
 #include <vector>
-#include <map>
-#include "Vector2i.h"
-#include "TileMap.h"
+#include <array>
+#include <set>
+#include <queue>
 #include "TileMaster.h"
+#include "Vector2i.h"
+
+
 
 class Pathfinding {
+    public:
+        struct Node {
+            Vector2i position;
+            Node* parent;
+            int GCost; // Cost from start to this node
+            int HCost; // Heuristic cost to end
+            int FCost() const { return GCost + HCost; }
+        
+            Node(Vector2i pos, Node* parentNode) : position(pos), parent(parentNode), GCost(0), HCost(0) {}
+        };
     public:
         Pathfinding(TileMaster* tMaster) : tileMaster(tMaster);
         ~Pathfinding();
