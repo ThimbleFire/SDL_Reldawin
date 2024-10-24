44834458eee514391f816d0187ea1de08ce3c03a
AlwaysEast <lojith@proton.me>
2024-10-24 01:25:24 +0100

    Update Pathfinding.h

--- src/Engine/Pathfinding.h
+++ src/Engine/Pathfinding.h
@@ -11,6 +11,9 @@ class Pathfinding {
         Pathfinding(std::map<Vector2i, TileMap*> maps);
         ~Pathfinding();
         void GetPath();
+
+    public:
+        std::array<TileMap*, 9> maps;
 };
 
 extern Pathfinding g_pathfinder;
